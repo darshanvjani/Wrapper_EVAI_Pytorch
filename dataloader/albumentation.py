55b7ca16e879e04abd70f039acb08ce04109e21b
darshanvjani <35656144+darshanvjani@users.noreply.github.com>
2021-08-07 03:56:36 -0700

    path wrapper

--- dataloader/albumentation.py
+++ dataloader/albumentation.py
@@ -1,7 +1,7 @@
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
 import numpy as np
-from boiler_code_pytorch.utils import helper
+from Wrapper_EVAI_Pytorch.utils import helper
 import torchvision.transforms as transforms
 
 class CIFAR10Albumentation:
